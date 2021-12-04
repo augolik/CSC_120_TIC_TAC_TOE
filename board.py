3a84f07f945b74e1877949d9ca8ffc2d52ae6701
Anya U <95142462+augolik@users.noreply.github.com>
2021-12-04 11:59:59 -0500

    board.py
    Stage 1 of Tic Tac Toe board -- printing board

---
+++ board.py
@@ -0,0 +1,16 @@
+board = [['','-','-']
+        ,['-','-','-']
+        ,['-','-','-']]
+
+#row = int(input('Enter Row: '))
+#column = int(input('Enter Column: '))
+#player = 0
+
+def printBoard():
+        print(board[0])
+        print(board[1])
+        print(board[2])
+
+
+
+printBoard()
